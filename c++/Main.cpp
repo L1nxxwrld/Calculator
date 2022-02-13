bfec9bceb58f6cb618c296749db407f0e1fa84df
micha <micha@L1NXXTOKOKS09>
2022-02-13 12:35:55 +0100

    Dodaj pliki projektów.

---
+++ c++/Main.cpp
@@ -0,0 +1,52 @@
+﻿// c++.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include "Calculator.h"
+
+using namespace std;
+int main()
+{
+    double x = 0.0;
+    double y = 0.0;
+    double result = 0.0;
+    char oper = '+';
+    
+    std::cout << "-----------------------------------------------------------" << endl << endl;
+    std::cout << "C++ Calculator" << endl << endl;
+    std::cout << "What you want to count?( + | - | * | /)"
+        << endl;
+    std::cout << "Write only numbers and ,+,-,*,/" << endl << endl;
+    std::cout << "Example: 100+50" << endl << endl;
+    std::cout << "-----------------------------------------------------------" << endl << endl;
+
+    Calculator c;
+    while (true)
+    {
+        cin >> x >> oper >> y;
+        if (oper == '/' && y == 0)
+        {
+            cout << "You can't divide by 0" << endl;
+            continue;
+        }
+        else
+        {
+            result = c.Calculate(x, oper, y);
+        }
+        cout << "Result is: " << result << endl;
+         }
+
+    return 0;
+    
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
